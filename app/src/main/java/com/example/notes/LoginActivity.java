39733e24d78a310a810c9b5421440f66829f48f0
yourajeet17 <yourajeet17@gmail.com>
2020-03-04 18:49:23 +0530

    updated

--- app/src/main/java/com/example/notes/LoginActivity.java
+++ app/src/main/java/com/example/notes/LoginActivity.java
@@ -1,25 +1,52 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import butterknife.BindView;
+import com.example.notes.Fragment.HomeFragment;
+import com.example.notes.Fragment.NotificationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         ButterKnife.bind(this);
+        bottomNavigationView=findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                Fragment fragment=null;
+                switch (item.getItemId()){
+                    case R.id.home:
+                        fragment=new HomeFragment();
+                        break;
+                    case R.id.dashboard:
+                        fragment=new DashboardFragment();
+                        break;
+                    case R.id.notification:
+                        fragment=new NotificationFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragcontainer,fragment).commit();
+                return;
+
+            }
+
+        });
 
         Intent intent=getIntent();
         if(intent!=null)
